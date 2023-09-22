9711dfef48ef7f7c22a425d7236281f7690fb614
abdullah-faik <abdalahfaik80@gmail.com>
2023-09-22 12:04:11 +0300

    FIX_BETTY

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -9,27 +9,30 @@
  */
 void insertion_sort_list(listint_t **list)
 {
-    listint_t *temp, *cur, *node;
+	listint_t *temp, *cur, *node;
 
-    cur = *list;
-    while (cur != NULL)
-    {
-        node = cur;
-        while (node->prev != NULL && node->prev->n > node->n)
-        {
-            temp = node->prev;
-            if (temp->prev != NULL)
-                temp->prev->next = node;
-            temp->next = node->next;
-            node->next = temp;
-            node->prev = temp->prev;
-            temp->prev = node;
-            if (temp->next != NULL)
-                temp->next->prev = temp;
-            if (node->prev == NULL)
-                *list = node;
-            print_list(*list);
-        }
-        cur = cur->next;
-    }
+	cur = *list;
+	if (*list == NULL)
+		return;
+
+	while (cur != NULL)
+	{
+		node = cur;
+		while (node->prev != NULL && node->prev->n > node->n)
+		{
+			temp = node->prev;
+			if (temp->prev != NULL)
+				temp->prev->next = node;
+			temp->next = node->next;
+			node->next = temp;
+			node->prev = temp->prev;
+			temp->prev = node;
+			if (temp->next != NULL)
+				temp->next->prev = temp;
+			if (node->prev == NULL)
+				*list = node;
+			print_list(*list);
+		}
+		cur = cur->next;
+	}
 }
