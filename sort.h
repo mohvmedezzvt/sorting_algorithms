85c464110b536d4f4799034afe9459061c7caf45
Mohamed Ezzat <mohamedelhdary321@gmail.com>
2023-09-23 04:56:11 +0300

    Added 2-selection_sort.c, 2-main.c

--- sort.h
+++ sort.h
@@ -22,8 +22,10 @@ typedef struct listint_s
 /* Assistance Functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-void insertion_sort_list(listint_t **list);
+
 /* Mandatory Functions */
 void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif /* SORT_H */
